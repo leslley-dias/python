b4bb4cc4df4b8aeb99efd886e6cc26e5c1072aeb
Leslley Dias <iaferro@gmail.com>
2020-01-20 18:08:46 -0200

    aula1

---
+++ Aulas/Exercicios/Aulas01/exer001.py
@@ -0,0 +1,13 @@
+#Calculo de Media 
+
+# n1 = float(input("Digite o valor da primeira nota: "))
+# n2 = float(input("Digite o valor da seguda nota: "))
+# n3 = float(input("Digite o valor da terceira nota "))
+# n4 = float(input("Digite o valor da quarta nota "))
+
+# media = (n1+n2+n3+n4) /4
+
+# print(media)
+
+produtos = ["camiseta",['p', 'm', 'g'], 'Calça', ['40', '42 ', '48' ], ['branca', 'amarela', 'verde']]
+  
---
+++ Aulas/Exercicios/Aulas01/exer01.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+#Números e operadores
+
+# print(10+15)
+# print(30-15)
+# print(10*5)
+# print(10/2)
+# print(10//3)
+# print(10%3)
+
+
+
+#Atribuições e variaveis
+
+# x=15
+# y=25
+
+# print(x<=y) 
+# print(x>=y)
+# print(x>y)
+# print(x<y)
+
+
+#Entrada e saida de dados
+
+# entrada= input("Digite a entrada:")
+# print(entrada)
+
+
+# x = int(input("Digite o valor de X: "))
+# y = int(input("Digite o valor de Y: "))
+
+# print(x + y )
+
+#Estruturas de dados
+
+#Listas
+
+#lista = [1, 1.6, 'strings', True, [1,2,[6,7,8,[9,10,11]], 4,5]]
+
+#Fatiamento de lista
+
+# print(lista[4][2][3])
+# print(lista[2:4])
+#print(lista[4].index(2))
+
+
+#metodos de listas
+# lista.append(100)
+# lista.insert()
+# print(lista)
+
+produtos = ["camiseta",['p', 'm', 'g'], 'Calça', ['40', '42 ', '48' ], ['branca', 'amarela', 'verde']]
+
+print(produtos[0]+', '+produtos[4][0]+', '+produtos[1][2])
+print(produtos[2],produtos[3][1], produtos[4][2])
+print(produtos[0],produtos[4][0], produtos[3][1])
